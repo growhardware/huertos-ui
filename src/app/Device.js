af0e8558a95d028db3653a2f4d72f01adc437261
Federico Mello <fedemello@gmail.com>
2020-09-30 19:24:19 -0300

    va device

---
+++ src/app/Device.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+/*
+class createDevice extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            What kind of device?<br/>
+            <input type="text" value={this.state.kind} onChange={(e) => this.setState({kind: e.target.value})} />
+
+         </label>
+        </form>
+      </div>
+    );
+  }
+} 
+*/
+
+class Device extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      settings: {},
+    };
+    this.handleSettingsChange = this.handleSettingsChange.bind(this);
+  }
+  handleSettingsChange(event) {this.setState({settings:{led: event.target.value}});}
+  render() {
+    return (
+      <div>
+        <label>
+          kind:<br/>
+          <label>{this.props.kind}</label>
+          alias:<br/>
+          <label>{this.props.alias}</label>
+          status:<br/>
+          led: <label>{this.props.status.led}</label>
+          pushButton: <label>{this.props.status.pushButton}</label>
+        </label>
+        <input type="submit" value="Submit" /> 
+      </div>
+    );
+  }
+}
+
+export default Device;
