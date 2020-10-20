1388410a31ddc2f9525d5cf9545d423d2a7e97d3
Hernan <hpereyradaluz@gmail.com>
2020-10-19 21:04:28 -0300

    app structure

--- src/app/components/Dashboard.js
+++ src/app/components/Dashboard.js
@@ -1,56 +1,69 @@
 import React from 'react'
 import Device from './Device.js'
-var _ = require('underscore');
+var _ = require('underscore')
 
 var Devices = [
-  {id:'5f3e9f3571afb3264ecd8dbe', kind:'ioTest', alias:'La Cuca', status:{led: false, pushButton: false},settings:{}},
-  {id:'y', kind:'ioTest', alias:'Otra C', status:{led: false, pushButton: false},settings:{}},
-];
+  {
+    id: '5f3e9f3571afb3264ecd8dbe',
+    kind: 'ioTest',
+    alias: 'La Cuca',
+    status: { led: false, pushButton: false },
+    settings: {},
+  },
+  {
+    id: 'y',
+    kind: 'ioTest',
+    alias: 'Otra C',
+    status: { led: false, pushButton: false },
+    settings: {},
+  },
+]
 
 class Dashboard extends React.Component {
   constructor(props) {
-    super(props);
-    this.getDevices = this.getDevices.bind(this);
-    this.handleResponse = this.handleResponse.bind(this);
-    this.handleSocket = this.handleSocket.bind(this);
+    super(props)
+    this.getDevices = this.getDevices.bind(this)
+    this.handleResponse = this.handleResponse.bind(this)
+    this.handleSocket = this.handleSocket.bind(this)
     this.state = {
       isLoggedIn: false,
       devices: Devices,
-    };
+    }
   }
   handleSocket(msg) {
-    console.log('Sails was sent a message: ', msg);
-    let devices = this.state.devices;
-    const index = _.findIndex(devices,{id:msg.id});
-    if(msg.data.status !== undefined){
-      let dev = devices[index];
-      dev.status = msg.data.status;
-      devices[index] = dev;
-      this.setState({devices: devices});
+    console.log('Sails was sent a message: ', msg)
+    let devices = this.state.devices
+    const index = _.findIndex(devices, { id: msg.id })
+    if (msg.data.status !== undefined) {
+      let dev = devices[index]
+      dev.status = msg.data.status
+      devices[index] = dev
+      this.setState({ devices: devices })
     }
   }
   getDevices() {
-    this.props.api.socket.get('/device/get-my-devices', this.handleResponse);
+    this.props.api.socket.get('/device/get-my-devices', this.handleResponse)
   }
   handleResponse(msg) {
-    console.log(msg);
-    this.setState({devices: msg});
+    console.log(msg)
+    this.setState({ devices: msg })
   }
-  componentDidMount(){
-    this.props.api.socket.on('device', this.handleSocket);
-    this.getDevices();
+  componentDidMount() {
+    this.props.api.socket.on('device', this.handleSocket)
+    this.getDevices()
   }
   render() {
-    const device = this.state.devices[0];
+    const device = this.state.devices[0]
     return (
       <div>
         <h2>Your Dashboard:</h2>
         <h4>Devices:</h4>
-        <div><Device api={this.props.api} document={device}/></div>          
+        <div>
+          <Device api={this.props.api} document={device} />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default Dashboard;
-  
+export default Dashboard
--- src/app/components/Device.js
+++ src/app/components/Device.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'
 import Switch from './Switch.js'
-import './App.css';
+import '../App.css'
 
 /*
 class createDevice extends React.Component {
@@ -21,11 +21,11 @@ class createDevice extends React.Component {
 */
 
 class Device extends React.Component {
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = {
       settings: {},
-    };
+    }
     // this.handleSettingsChange = this.handleSettingsChange.bind(this);
   }
   render() {
@@ -33,16 +33,25 @@ class Device extends React.Component {
       <div className="Device">
         <label>
           <label>{this.props.document.alias}</label>
-          <label>{' ('+this.props.document.kind+')'}</label>
-          <br/>
-          <br/>status:
-          <br/>pushButton: <label>{this.props.document.status.pushButton ? 'ON' : 'OFF'}</label>
-          <br/>led: <label>{this.props.document.status.led ? 'ON' : 'OFF'}</label>
+          <label>{' (' + this.props.document.kind + ')'}</label>
+          <br />
+          <br />
+          status:
+          <br />
+          pushButton:{' '}
+          <label>{this.props.document.status.pushButton ? 'ON' : 'OFF'}</label>
+          <br />
+          led: <label>{this.props.document.status.led ? 'ON' : 'OFF'}</label>
         </label>
-        <br/><Switch api={this.props.api} id={this.props.document.id} on={this.props.document.status.led}></Switch>
+        <br />
+        <Switch
+          api={this.props.api}
+          id={this.props.document.id}
+          on={this.props.document.status.led}
+        ></Switch>
       </div>
-    );
+    )
   }
 }
 
-export default Device;
+export default Device
