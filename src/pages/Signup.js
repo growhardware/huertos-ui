38db359bf66f391a02c70e87dc6380e813a0d167
Hernán Pereyra <hpereyra@switch.local>
2021-08-26 08:59:14 -0300

    fix wtf

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 import { useAuth } from '../contexts/AppContext'
 import { useHistory, useLocation } from 'react-router-dom'
-import { signUp } from '../services/authService'
 
 export default function Signin() {
     let history = useHistory();
@@ -22,7 +21,6 @@ export default function Signin() {
         history.replace(from);
     };
     const onSubmit = data => {
-        signUp(data)
         console.log(data);
         auth.signup(data, callback);
     }
