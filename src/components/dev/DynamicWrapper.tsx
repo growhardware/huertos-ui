92522c17d094744c10ade6b39afd581000c43533
Hernán Pereyra <hernan.pereyra@modelit.xyz>
2022-06-21 20:07:57 -0300

    svelte ui migration, first approach

--- src/components/dev/DynamicWrapper.tsx
+++ src/components/dev/DynamicWrapper.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+// import React from 'react';
 
-interface DynamicComponentProps {
-  text: string;
-}
+// interface DynamicComponentProps {
+//   text: string;
+// }
 
-const DynamicComponent: React.FC<DynamicComponentProps> = ({ text }) => {
-  return <div>{text}</div>;
-};
+// const DynamicComponent: React.FC<DynamicComponentProps> = ({ text }) => {
+//   return <div>{text}</div>;
+// };
 
-const createDynamicComponent = (component: React.ComponentType<any>, props: any) => {
-  return React.createElement(component, props);
-};
+// const createDynamicComponent = (component: React.ComponentType<any>, props: any) => {
+//   return React.createElement(component, props);
+// };
 
-const DynamicWrapper: React.FC = () => {
-  const dynamicComponent = createDynamicComponent(DynamicComponent, {});
-  return <div>{dynamicComponent}</div>;
-};
+// const DynamicWrapper: React.FC = () => {
+//   const dynamicComponent = createDynamicComponent(DynamicComponent, {});
+//   return <div>{dynamicComponent}</div>;
+// };
 
-export default DynamicWrapper;
+// export default DynamicWrapper;
+//.tsx
