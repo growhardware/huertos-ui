a49cff1c33e780206c4f40fe9d274122ddc2d0e8
ganjamentor <fedemello@gmail.com>
2022-07-02 21:10:22 -0300

    All status and history reactive on device card.

--- src/components/HistoryChart.svelte
+++ src/components/HistoryChart.svelte
@@ -1,8 +1,7 @@
 
 <script>
     // TODO ------>
-    // with the tasks for the persited and rehydrated store with xsvelte,
-    // redising a solution to make the history chart using xsvelte
+
     export let history;
     export let tag;
 </script>
--- src/components/dashboard.svelte
+++ src/components/dashboard.svelte
@@ -6,21 +6,23 @@ import AddDeviceModal from './AddDeviceModal.svelte';
 // import CreateDevice from '../components/CreateDevice.svelte'
 
     let state = {};
-    const handleSocket = (msg) => {
-        console.log('Sails was sent a message: ', msg)
-        const devices = _.toArray(state.devices)
-        const index = _.findIndex(devices, { id: msg.id })
-        
-        if (msg.data.status !== undefined) {
-            let dev = devices[index]
-            dev['status'] = msg.data.status
-            dev['history'] = msg.previous.history
-            devices[index] = dev
-        }
-        setState(devices)
-    }
+
     const setState = (msg) => {
         msg?.length > 0 ? state.devices = [...msg] : msg?.devices?.length > 0 ? state.devices = [...msg.devices] : state.devices
+    }
+
+    const handleSocket = (msg) => {
+        console.log('Sails was sent a message: ', msg)
+        const devices = state.devices
+        const index = devices.findIndex((d) => {if(d.id==msg.id){ return true; }})
+        let device = devices[index]    
+        if (msg.data.status !== undefined) {
+            device['status'] = msg.data.status
+            const event = {'createdAt':msg.updatedAt, 'status':msg.data.status}
+            device.history.push(event)
+            devices[index] = device
+        }
+        setState(devices)
     }
     
     const getDevices = () =>{
@@ -28,15 +30,18 @@ import AddDeviceModal from './AddDeviceModal.svelte';
     }
 
     const handleResponse = (msg) => {
+        console.log(msg)
         setState({ devices: msg })
     }
     
     api.socket.on('device', handleSocket);
+
     getDevices();
     
     $: deviceList = state.devices
 
     let searchTerm = "my device";
+
 </script>
 
 <div>
