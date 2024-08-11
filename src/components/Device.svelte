f4340b56b5a5d8f63b85ff396b76bdb08fce89e6
IA3 <ingenieriart3@gmail.com>
2024-08-11 15:06:29 -0300

    rebase commit

--- src/components/Device.svelte
+++ src/components/Device.svelte
@@ -4,18 +4,11 @@
     import Switch from '../components/Switch.svelte'
     export let device;
 
-    const switchProp = device.status.switch
-    $: led = device.status.led
-    const history = device.history
-    const switchHistory = history.filter( r => r.status.hasOwnProperty('switch') )
+    $: status = device.status
+    $: switchHistory = device.history.filter( r => r.status.hasOwnProperty('switch') )
     // console.log(switchHistory)
-    const ledHistory = history.filter( r => r.status.hasOwnProperty('led') )
+    $: ledHistory = device.history.filter( r => r.status.hasOwnProperty('led') )
 
-    // $: led = device.settings.led // no preciso definir asi
-
-    $:{
-        // console.log(' veo led ', led)
-    }
 </script>
 <div class="max-w-sm rounded overflow-hidden shadow-lg float-left">
     <div class="p-8 ... text-gray-700 text-base my-4 ...">
@@ -25,12 +18,12 @@
         <br />
         kind: {device.kind}
         <br />
-        led status: <div class={device.status.led? "bg-yellow-400" : "bg-blue-600"}>{device.status.led ? ' ON' : ' OFF'}</div>
+        led status: <div class={status.led? "text-yellow-400" : "text-blue-600"}>{status.led? ' ON' : ' OFF'}</div>
         led history: <HistoryChart history={ledHistory} tag={'led'} />
         <br />
-        switch status: <div class={device.status.switch? "bg-yellow-400" : "bg-blue-600"}>{device.status.switch ? ' ON' : ' OFF'}</div>
+        switch status: <div class={status.switch? "text-yellow-400" : "text-blue-600"}>{status.switch? ' ON' : ' OFF'}</div>
         switch history: <HistoryChart history={switchHistory} tag={'switch'}/>
         <br />
-        <Switch api={api} id={device.id} on={!led} name={'led'}/>
+        <Switch api={api} id={device.id} on={!status.led} name={'led'}/>
     </div>
 </div>
---
+++ src/components/HistoryChart.svelte
@@ -0,0 +1,11 @@
+
+<script>
+    // TODO ------>
+
+    export let history;
+    export let tag;
+</script>
+
+<div>
+    the HistoryChartnnel {history}, tag {tag}
+</div>
