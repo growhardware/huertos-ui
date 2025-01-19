d3170cba9e85b39950f17ebe0ff6b846b5aa9eb8
IA3 <ingenieriart3@gmail.com>
2025-01-18 23:09:00 -0300

    refactoring code, improving device crud

--- src/pages/Device/DeviceUpdate.tsx
+++ src/pages/Device/DeviceUpdate.tsx
@@ -2,81 +2,71 @@ import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
-import {
-  updateDevice,
-  getDevice,
-  deleteDevice,
-} from '../../services/device-service';
+import { updateDevice, getDevice } from '../../services/device-service';
 import { DevicesSvg } from '../../components/Svg/DevicesSvg';
 import { useNavigate } from 'react-router-dom';
-// import io from '../../services/socket';
+
 const DeviceUpdate = () => {
-  const { id } = useParams();
-  const [selectedOption, setSelectedOption] = useState<string>('');
-  const [isOptionSelected, setIsOptionSelected] = useState<boolean>(false);
-  const [data, setData] = useState([]);
-  const [alias, setAlias] = useState(); // use `undefined` value
-  const [kind, setKind] = useState(); // use `undefined` value
-  const [port, setPort] = useState(); // use `undefined` value
-  const [status, setStatus] = useState(); // use `undefined` value
-  const [plan, setSettings] = useState(); // use `undefined` value
   const navigate = useNavigate();
+  const { id } = useParams();
+  const [isOptionSelected, setIsOptionSelected] = useState<boolean>(false);
 
-  function handleChangeAlias(event) {
-    setAlias(event.target.value);
-  }
+  const [deviceData, setDeviceData] = useState({
+    alias: '',
+    kind: '',
+    port: '',
+    status: '',
+    plan: '',
+  });
 
-  function handleChangeKind(event) {
-    setKind(event.target.value);
-  }
+  useEffect(() => {
+    const fetchDevice = async () => {
+      try {
+        const response = await getDevice(id, handleResponse);
+        console.log('RESPONSEEEEE ', response);
+        if (response.statusCode === 200) {
+          const { alias, kind, port, status, plan } = response.body;
+          setDeviceData({ alias, kind, port, status, plan });
+        } else {
+          console.error('Error fetching device:', response);
+        }
+      } catch (error) {
+        console.error('Error fetching device:', error);
+      }
+    };
 
-  function handleChangePort(event) {
-    setPort(event.target.value);
-  }
+    fetchDevice();
+  }, [id]);
 
-  function handleChangeStatus(event) {
-    setStatus(event.target.value);
-  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDeviceData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-  function handleChangeSettings(event) {
-    setSettings(event.target.value);
-  }
   const handleResponse = (body, JWR) => {
     console.log('body ', body);
     if (JWR.statusCode === 200) {
-      // data = body;
-      setData(body);
-      setAlias(body.alias);
-      setKind(body.kind);
-      setPort(body.port);
-      setStatus(body.status);
-      setSettings(body.plan);
+      const { alias, kind, port, status, plan } = body;
+      setDeviceData({ alias, kind, port, status, plan });
       console.log('hr getDevice ', body);
-      // return body;
-      //     // setState({creating: false});
-      //     // props.onCreated();
     } else {
       console.log('Error: ', JWR);
     }
   };
 
-  const hChange = (e) => {
-    data.alias = e.target.value;
-  };
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        await getDevice(id, handleResponse);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-    fetchData();
-  }, []);
-
-  const changeTextColor = () => {
-    setIsOptionSelected(true);
-  };
+  // const hChange = (e) => {
+  //   data.alias = e.target.value;
+  // };
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       await getDevice(id, handleResponse);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
 
   const {
     register,
@@ -85,14 +75,13 @@ const DeviceUpdate = () => {
   } = useForm();
 
   const onSubmit = () => {
-    const status = {};
     const attributes = {
       id: id,
-      alias: alias,
-      kind: kind,
-      port: port,
-      status: status,
-      plan: plan,
+      alias: deviceData.alias,
+      kind: deviceData.kind,
+      port: deviceData.port,
+      status: deviceData.status,
+      plan: deviceData.plan,
     };
     updateDevice(attributes);
     navigate(`/devices/`, { replace: true });
@@ -128,8 +117,9 @@ const DeviceUpdate = () => {
                   <input
                     type="text"
                     placeholder="Alias"
-                    value={alias}
-                    onChange={handleChangeAlias}
+                    name="alias"
+                    value={deviceData.alias}
+                    onChange={handleChange}
                     // {...register('alias', { required: true })}
                     className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                   />
@@ -139,21 +129,12 @@ const DeviceUpdate = () => {
                   <label className="mb-3 block text-black dark:text-white">
                     Kind
                   </label>
-                  {/* <input
-                    type="text"
-                    placeholder="Kind"
-                    /{...register('kind', { required: true })}
-                    className="w-full rounded-lg border-[1.5px] border-primary bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:bg-form-input dark:text-white"
-                  /> */}
                   <select
                     placeholder="Kind"
                     {...register('kind', { required: true })}
-                    value={kind ? kind : selectedOption}
-                    onChange={handleChangeKind}
-                    // onChange={(e) => {
-                    //   setSelectedOption(e.target.value);
-                    //   changeTextColor();
-                    // }}
+                    name="kind"
+                    value={deviceData.kind}
+                    onChange={handleChange}
                     className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-12 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input ${
                       isOptionSelected ? 'text-black dark:text-white' : ''
                     }`}
@@ -187,8 +168,9 @@ const DeviceUpdate = () => {
                   <input
                     type="text"
                     placeholder="Port"
-                    value={port}
-                    onChange={handleChangePort}
+                    name="port"
+                    value={deviceData.port}
+                    onChange={handleChange}
                     // {...register('port', { required: true })}
                     className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary dark:disabled:bg-black"
                   />
@@ -201,34 +183,24 @@ const DeviceUpdate = () => {
                   <input
                     type="text"
                     placeholder="Status"
-                    value={JSON.stringify(status)}
-                    onChange={handleChangeStatus}
+                    name="status"
+                    value={JSON.stringify(deviceData.status)}
+                    onChange={handleChange}
                     // {...register('status', { required: true })}
                     className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary dark:disabled:bg-black"
                   />
                 </div>
 
-                {/* <div>
-                  <label className="mb-3 block text-black dark:text-white">
-                    Status
-                  </label>
-                  <textarea
-                    rows={6}
-                    placeholder="Active textarea"
-                    {...register('status', { required: true })}
-                    className="w-full rounded-lg border-[1.5px] border-primary bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:bg-form-input dark:text-white"
-                  ></textarea>
-                </div> */}
-
                 <div>
                   <label className="mb-3 block text-black dark:text-white">
-                    Settings
+                    Plan
                   </label>
                   <textarea
                     rows={6}
                     placeholder="Active textarea"
-                    value={JSON.stringify(plan)}
-                    onChange={handleChangeSettings}
+                    name="plan"
+                    value={JSON.stringify(deviceData.plan)}
+                    onChange={handleChange}
                     // {...register('plan', { required: true })}
                     className="w-full rounded-lg border-[1.5px] border-primary bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:bg-form-input dark:text-white"
                   ></textarea>
